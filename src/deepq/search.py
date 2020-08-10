85c78d7015a19837b98012b96fcd1eb08f4f6e9f
zhoubin-me <zhoubin.me@gmail.com>
2020-08-11 01:54:06 +0800

    update searcher

--- src/deepq/search.py
+++ src/deepq/search.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         metric_columns=["game", "frames", "loss", "ep_reward_test", "ep_reward_train", "ep_reward_test_max",
                         "time_past", "time_remain", "speed", "epsilon", "qmax"])
 
+    kwargs['total_steps'] = int(2e7)
     analysis = tune.run(
         Trainer,
         name=kwargs['exp_name'],
@@ -36,10 +37,10 @@ if __name__ == '__main__':
         stop=lambda trial_id, result: result['frames'] > kwargs['total_steps'],
         checkpoint_freq=1000,
         config=dict(
-            game="Enduro",
-            agent_train_freq=tune.grid_search([15, 10]),
-            exploration_ratio=tune.grid_search([0.1, 0.15]),
             # "game": tune.grid_search(['BeamRider', 'Qbert', 'Enduro', 'Riverraid']),
+            game=tune.grid_search(['Breakout', 'Pong', 'SpaceInvaders', 'Seaquest']),
+            epoches=kwargs['total_steps'] // int(1e4),
+            total_steps=kwargs['total_steps'],
         ),
         progress_reporter=reporter,
         resources_per_trial={"gpu": 3},
