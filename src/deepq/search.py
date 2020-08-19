08b1d2622deb81a98e70038273ddba50f2672691
zhoubin-me <zhoubin.me@gmail.com>
2020-08-19 23:48:17 +0800

    fix

--- src/deepq/search.py
+++ src/deepq/search.py
@@ -2,10 +2,8 @@ import argparse
 
 import git
 import ray
-from mlflow.tracking import MlflowClient
 from ray import tune
 from ray.tune import CLIReporter
-from ray.tune.logger import MLFLowLogger, DEFAULT_LOGGERS
 
 from src.deepq.config import Config
 from src.deepq.trainer import Trainer
@@ -22,16 +20,13 @@ def parse_arguments(config):
 def trial_str_creator(trial, sha):
     return "{}_{}_{}_{}".format(trial.trainable_name, trial.config['game'], sha, trial.trial_id)
 
-
 if __name__ == '__main__':
     cfg = Config()
     cfg = parse_arguments(cfg)
-    ray.init(memory=20 * 2 ** 30, object_store_memory=80 * 2 ** 30)
-    client = MlflowClient()
 
+    ray.init(memory=20 * 2 ** 30, object_store_memory=80 * 2 ** 30)
     repo = git.Repo(search_parent_directories=True)
     sha = repo.git.rev_parse(repo.head.object.hexsha, short=True)
-    experiment_id = client.create_experiment(cfg.exp_name + '_' + sha)
 
     reporter = CLIReporter(
         metric_columns=["game", "frames", "loss", "ep_reward_test", "ep_reward_train", "ep_reward_test_max",
@@ -46,7 +41,6 @@ if __name__ == '__main__':
         restore=cfg.restore_ckpt,
         stop=lambda trial_id, result: result['frames'] > cfg.total_steps,
         checkpoint_freq=1000,
-        loggers=DEFAULT_LOGGERS + (MLFLowLogger,),
         trial_name_creator=tune.function(lambda trial: trial_str_creator(trial, sha)),
         config=dict(
             game=tune.grid_search(
@@ -58,7 +52,6 @@ if __name__ == '__main__':
             noisy=cfg.noisy,
             num_atoms=cfg.default_num_atoms(),
             random_seed=cfg.random_seed,
-            logger_config=dict(mlflow_experiment_id=experiment_id),
         ),
         progress_reporter=reporter,
         resources_per_trial={"gpu": 1, "extra_gpu": 1},
