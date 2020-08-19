729380fc2b6824458d9796cf551bd8fea6643d02
zhoubin-me <zhoubin.me@gmail.com>
2020-08-19 14:43:25 +0800

    fix

--- src/deepq/search.py
+++ src/deepq/search.py
@@ -17,6 +17,10 @@ def parse_arguments(config):
     return Config(**vars(args))
 
 
+def trial_str_creator(trial, sha):
+    return "{}_{}_{}".format(trial.trainable_name, sha, trial.trial_id)
+
+
 if __name__ == '__main__':
     cfg = Config()
     cfg = parse_arguments(cfg)
@@ -25,7 +29,10 @@ if __name__ == '__main__':
     reporter = CLIReporter(
         metric_columns=["game", "frames", "loss", "ep_reward_test", "ep_reward_train", "ep_reward_test_max",
                         "ep_reward_train_max", "time_past", "time_remain", "speed", "epsilon", "qmax"])
+
     repo = git.Repo(search_parent_directories=True)
+    sha = repo.git.rev_parse(repo.head.object.hexsha, True)
+
     analysis = tune.run(
         Trainer,
         name=cfg.exp_name,
@@ -35,6 +42,7 @@ if __name__ == '__main__':
         restore=cfg.restore_ckpt,
         stop=lambda trial_id, result: result['frames'] > cfg.total_steps,
         checkpoint_freq=1000,
+        trial_name_creator=tune.function(lambda trial: trial_str_creator(trial, sha)),
         config=dict(
             game=tune.grid_search(
                 ['Breakout', 'Enduro', 'Seaquest', 'BeamRider', 'Pong', 'Asterix', 'Qbert', 'SpaceInvaders']),
@@ -44,7 +52,6 @@ if __name__ == '__main__':
             distributional=cfg.distributional,
             noisy=cfg.noisy,
             num_atoms=cfg.default_num_atoms(),
-            sha=repo.head.object.hexsha,
         ),
         progress_reporter=reporter,
         resources_per_trial={"gpu": 1, "extra_gpu": 1},
