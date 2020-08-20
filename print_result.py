4a2bd4259b09e2a51c06101899c9a203cef13936
John Smith <john@smith.com>
2020-08-20 10:37:30 +0800

    .

--- print_result.py
+++ print_result.py
@@ -16,9 +16,8 @@ for f in fs:
     rs = np.array(data['ITRs'])
     frames = data['frame_count']
     exp_name = f.split('/')[-3]
-    with open(f[:-9] + 'params.json', 'r') as fp:
+    with open(f[:-8] + 'params.json', 'r') as fp:
         game = json.load(fp)['game']
-
     print(
-        f"{myhost:>15s} {exp_name:>15s} {game:>15s} {data.mean():8.2f}\t "
-        f"{data.std():8.2f}\t {data.max():8.2f}\t {data.min():8.2f}\t {data.size:8.2f}\t {frames}")
+        f"{myhost:>15s} {exp_name:>15s} {game:>15s} {rs.mean():8.2f}\t "
+        f"{rs.std():8.2f}\t {rs.max():8.2f}\t {rs.min():8.2f}\t {rs.size:8.2f}\t {frames}")
