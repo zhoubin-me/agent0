b40087ce2f3a89da29969c17db6b307ffa227b28
zhoubin-me <zhoubin.me@gmail.com>
2020-08-21 15:34:15 +0800

    add nstep

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-python -m src.deepq.search --exp_name atari_deepq15
-python -m src.deepq.search --exp_name atari_deepq12 --distributional True --num_atoms 51
-python -m src.deepq.search --exp_name atari_deepq14 --noisy True
-python -m src.deepq.search --exp_name atari_deepq16 --adamw False
+python -m src.deepq.search --exp_name atari_deepq12 --distributional True --num_atoms 51 --double_q True --dueling True
+python -m src.deepq.search --exp_name atari_deepq13 --double_q False --dueling True
+python -m src.deepq.search --exp_name atari_deepq14 --double_q True --dueling True --n_step 3
+python -m src.deepq.search --exp_name atari_deepq15 --double_q True --dueling True
+
+python -m src.deepq.search --exp_name atari_deepq16 --double_q True --dueling True --noisy True
+python -m src.deepq.search --exp_name atari_deepq17 --double_q True --dueling False
 
