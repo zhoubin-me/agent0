c0a5a7bdda7bf0e43b0c7829967f4803b49394ba
Zhou Bin <zhoubin.me@gmail.com>
2020-08-25 05:55:31 +0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-python -m src.deepq.search --exp_name atari_deepq12 --distributional True --num_atoms 51 --double_q True --dueling True
-python -m src.deepq.search --exp_name atari_deepq13 --double_q False --dueling True
-python -m src.deepq.search --exp_name atari_deepq14 --double_q True --dueling True --n_step 3
-python -m src.deepq.search --exp_name atari_deepq15 --double_q True --dueling True
-
-python -m src.deepq.search --exp_name atari_deepq16 --double_q True --dueling True --noisy True
-python -m src.deepq.search --exp_name atari_deepq17 --double_q True --dueling False
-
+python -m src.deepq.search --double_q --dueling --distributional --n_step 3 --game atari8 --exp_name atari_deepq19
+python -m src.deepq.search --double_q --dueling --noisy --distributional --n_step 3 --game atari8 --exp_name atari_deepq18
