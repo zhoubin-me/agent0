4d9fb5f3374124b5c7227758132ef832d19db4c6
Zhou Bin <zhoubin.me@gmail.com>
2020-08-27 18:11:16 +0800

    add MDQN

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-python -m src.deepq.search --double_q --dueling --prioritize --noisy --algo c51 --n_step 3 --game atari8 --exp_name atari_rainbow
+python -m src.deepq.run --algo dqn
+python -m src.deepq.run --algo mdqn
+python -m src.deepq.run --algo c51
+python -m src.deepq.run --algo qr
+
+
+
+
+
+
+
+
+
+# python -m src.deepq.run --double_q --dueling --prioritize --noisy --algo c51 --n_step 3 --game atari8 --exp_name atari_rainbow
