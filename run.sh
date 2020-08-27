ee3ffc29dbee617b17141a73713a3a9dc94a5620
Zhou Bin <e0264481@atlas8-c01.hpc.local>
2020-08-27 23:43:54 +0800

    .

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
-python -m src.deepq.run --game atari6 --algo dqn --exp_name atari_dqn
-python -m src.deepq.run --game atari6 --algo mdqn --exp_name atari_mdqn
-python -m src.deepq.run --game atari6 --algo qr --exp_name atari
-
-
-
-
-
-
-
-
-
+python -m src.deepq.run --game atari6 --algo all --exp_name atari_all
+# python -m src.deepq.run --game atari6 --algo dqn --exp_name atari_dqn
+# python -m src.deepq.run --game atari6 --algo mdqn --exp_name atari_mdqn
+# python -m src.deepq.run --game atari6 --algo qr --exp_name atari_qr
+# python -m src.deepq.run --game atari6 --algo c51 --exp_name atari_all
 # python -m src.deepq.run --double_q --dueling --prioritize --noisy --algo c51 --n_step 3 --game atari8 --exp_name atari_rainbow
