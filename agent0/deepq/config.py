43d5a31aa9f779f5d539f7c9063d3522e2ac20aa
Zhou Bin <zhoubin.me@gmail.com>
2020-09-27 22:04:56 +0800

    fix cor reg

--- agent0/deepq/config.py
+++ agent0/deepq/config.py
@@ -5,6 +5,7 @@ from agent0.common.bench import atari6, atari10, atari47, atari_exp7, atari63
 
 @dataclass
 class Config:
+    # algo setting
     game: str = ""
     double_q: bool = False
     dueling: bool = False
@@ -15,32 +16,48 @@ class Config:
     algo: str = 'dqn'
     # support: epsilon_greedy, soft_explore
     policy: str = 'epsilon_greedy'
+
+    # replace noisy layer to gmm layer. not working
     gmm_layer: bool = False
 
+    # opitimizer related
     adam_lr: float = 5e-4
     fraction_lr: float = 2.5e-8
+
+    # c51 related
     v_max: float = 10
     v_min: float = -10
     num_atoms: int = -1
+
+    # prioritize related
     priority_alpha: float = 0.5
     priority_beta0: float = 0.4
+
+    # mdqn related
     mdqn_tau: float = 0.03
     mdqn_alpha: float = 0.9
     mdqn_lo: float = -1
+
+    # IQR related
     K_iqr: int = 32
     N_iqr: int = 64
     N_iqr_dash: int = 64
     N_fqf: int = 32
-    gmm_num_samples: int = 25000
-    gmm_max_std: float = 3.0
     num_cosines: int = 64
     clip_grad_norm: float = -1
+
+    # gmm_related
+    gmm_num_samples: int = 25000
+    gmm_max_std: float = 3.0
+
+    # others
     cor_loss: bool = False
     cor_reg: float = 0.05
     best_ep: bool = False
     ep_best_len: int = 100000
     best_ep_reg: float = 0.01
 
+    # basic training setting
     batch_size: int = 400
     replay_size: int = int(1e6)
     discount: float = 0.99
