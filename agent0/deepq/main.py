948eb31ef66ae837bb3baf5b961a5e58841294ee
ZHOU Bin <zhoubin.me@gmail.com>
2023-07-11 23:12:26 +0800

    remove print

--- agent0/deepq/main.py
+++ agent0/deepq/main.py
@@ -10,12 +10,10 @@ from dacite import from_dict
 @hydra.main(version_base=None, config_name="config")
 def main(cfg: ExpConfig):
     cfg = from_dict(ExpConfig, cfg)
-    print(cfg)
     dummy_env = make_atari(cfg.env_id, num_envs=1)
     cfg.obs_shape = dummy_env.observation_space.shape[1:]
     cfg.action_dim = dummy_env.action_space[0].n
     dummy_env.close()
-    print(cfg)
     Trainer(cfg).run()
 
 
